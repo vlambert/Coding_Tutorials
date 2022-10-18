8c6d129ff0ff5f0ef64b02ffe1a9a6761e4f4096
Valere Lambert <valerelambert@gmail.com>
2022-10-18 16:53:19 -0700

    hello

---
+++ 1_IntroPython/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 18 13:40:33 2022
+
+@author: valere lambert
+"""
+
+def hello():
+    """Print  "Hello World" and return None """
+    
+    print("Hello World")
+ 
+        
+
+# Main call of function
+hello()
+
+# Things to consider implements:
+# 1) pass a string to the hello function (like a name) to print after "hello"
+# 2) implement a check to see if the string is a proper name and if not,
+#    request a new string
