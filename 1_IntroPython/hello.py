35496edb8915e2ee9a2722daba2598c9d0b5ed7b
travisalongi <travis.alongi@gmail.com>
2022-10-19 09:31:50 -0700

    Added a few more ideas. Some formatting

--- 1_IntroPython/hello.py
+++ 1_IntroPython/hello.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 18 13:40:33 2022
+Oct 2022.
 
-@author: valere lambert
+Let's build on what we learned so far.
+We'll work with functions, strings, itegers, booleans, etc.
+to write code to do fun things and incrementally write code
+
+Note: you may need to remove the first 2 lines of code for this to work
+on your machine.
+
+@author ⤳ Valere Lambert
 """
 
+
 def hello():
-    """Print  "Hello World" and return None """
-    
+    """Print  "Hello World" and return None."""
     print("Hello World")
- 
-        
+
 
 # Main call of function
 hello()
 
 # Things to consider implements:
 # 1) pass a string to the hello function (like a name) to print after "hello"
+
 # 2) implement a check to see if the string is a proper name and if not,
 #    request a new string
+
+# 3) Add a birth year to calculate the user's name and return it
+
+# 4) Store the users name and age in a variable as a tuple and write a new
+#    function that then calculates the year that the user will turn 21 or the
+#    year that user turned 21, use print to return a sensible message.
+
+# 5) Extend this function
